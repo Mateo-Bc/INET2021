78698e2f2561e9e977de679242ec3c5407f00f3c
Mateo-Bc <mateobecettecura12345@gmail.com>
2021-08-20 05:08:46 -0300

    Rossi se metió a las 5 de la mañana a mi casa, y desde mi compu hizo la vista 'home' completamente funcional, responsive y todo; este Rossi ya me está dando miedo

--- INET2021/views.py
+++ INET2021/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q
 
 # Create your views here.
 
 def HomeView(request):
     locals = Local.objects.all()
+
+    queryset = request.GET.get("buscar")
+    if queryset:
+        locals = Local.objects.filter(
+            Q(nomvre__icontains = queryset) |
+            Q(address__icontains = queryset)
+        ).distinct()
+
+    if (queryset == None):
+        queryset = ""
+
     context= {
-        'locales':locals,
+        'locales': locals,
+        'buscar': queryset,
     }
     return render(request, 'home.html' , context)
 
@@ -16,7 +29,7 @@ def Local_View(request, pk):
         'nomvre': local.nomvre,
         'max_cap': local.max_cap,
         'ac_cap': local.ac_cap,
-        'manager': local.manager
+        'direccion': local.address,
 
     }
     return render(request, 'local_view.html' , context)
